13068d9bd324d8a1379fea0751d1677d1b5b8c76
SgtBreadStick <sgtbreadstick16@gmail.com>
2018-04-05 23:13:42 +0100

    TruckersMP Status v2.0
    TruckersMP Status v2.0

---
+++ README.md
@@ -0,0 +1,48 @@
+# TruckersMP Status Project Version 2.0
+
+This TruckersMP Status Project can be used in many different ways. It includes
+* Bootstrap 3
+* TruckersMP API (Cached)
+* PHP
+* Font Awesome 4.7.1
+* Jquery
+
+## Getting Started
+
+Make sure to have these set in php.ini for this to work properly!
+* Any Of These PHP Versions 5.6 7.0 7.1
+* allow_url_fopen on (If Possible Or If Needed Or If cURL Isn't Being Used)
+
+### Notes
+
+You can remove the author section if you want to but do not put that you created it.
+It can be modified in any way.
+Thanks To StuartD to help with the caching.
+ 
+### How to use
+
+Make sure all folders with the index.php are in the same directory.
+If not in same directory then change the url of the sources in the index.php file.
+Make sure you did what was said in Getting Started.
+
+### Version 2.0
+
+This Project is currently on Version 2.0
+
+Changelog:
+v1.2
+* Now shows speedlimiter in more detail with the actual limiter speed.
+* New and better style
+* API is now cached so you no longer need to worry about that.
+* Added collisions (Thanks To Lasse)
+* Page now has fluid width and is now full width
+* Added version number to bottom of the page
+* The "api.sgtbreadstick.tk" is now the status URL for caching.
+v2.0
+* Now using cURL for better loading times.
+* Now v2.0 Due to many technical errors
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
---
+++ src/index.php
@@ -0,0 +1,87 @@
+<html>
+    <head>
+        <title>TruckersMP Status</title>
+        <link rel="icon" type="image/png" href="img/truckersmp-icon.png"/>
+        <meta name="viewport" content="initial-scale=1">
+        <link href="fontawesome/css/font-awesome.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/metro-bootstrap.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/jquery.dataTables.min.css" rel="stylesheet">
+    </head>
+<body>
+    <br>
+    <div class="container-fluid">
+        <strong>
+            <h1>TruckersMP Status</h1>
+        </strong><hr>
+        <div class="row">
+<?php
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://api.sgtbreadstick.tk/");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 1);
+    $servers = json_decode(curl_exec($curl), true);
+    
+        foreach($servers['response'] as $serv){
+            if($serv['online']){
+                $status = '<strong><font color="green">Online</font></strong>';
+            } else {
+                $status = '<strong><font color="red">Offline</font></strong>';
+            }
+            
+            if($serv['speedlimiter']){
+                $speedlimiter = '<font color="green">110KM/H</font>';
+            } else {
+                $speedlimiter = '<font color="red">150KM/H</font>';
+            }
+            
+            if($serv['collisions']){
+                $collisions = '<font color="green">Enabled</font>';
+            } else {
+                $collisions = '<font color="red">Disabled</font>';
+            }
+
+            if($serv['policecarsforplayers']){
+                $police = '<hr><i class="fa fa-car fa-3x" aria-hidden="true"></i><p class="text">Police For Players: <strong><font color="blue">Enabled</font></strong></p>';
+            } else {
+                $police = '';
+            }
+            
+            
+            echo '
+                    <div class="col-lg-4">
+                    <div class="panel panel-default">
+                      <div class="panel-heading">
+                        <h3 class="panel-title">'. $serv['name'] . ' - ' . $serv['game'] .'</h3>
+                      </div>
+                      <div class="panel-body" style="box-sizing: border-box; border: 10px solid #f2f2f2;">
+                        <i class="fa fa-users fa-3x fa-fw" aria-hidden="true"></i><p class="text">Players: <strong>'. $serv['players'] .'</strong></big><small> / '. $serv['maxplayers'] .'</strong></small></p><hr>
+                        <i class="fa fa-user-o fa-3x fa-fw" aria-hidden="true"></i><p class="text">Queue: <strong>'. $serv['queue'] .'</strong></p><hr>
+                        <i class="fa fa-server fa-3x fa-fw" aria-hidden="true"></i><p class="text">Server Status: <strong>'. $status .'</strong></p><hr>
+                        <i class="fa fa-times fa-3x fa-fw" aria-hidden="true"></i><p class="text">Collisions: <strong>'. $collisions .'</strong></p><hr>
+                        <i class="fa fa-tachometer fa-3x fa-fw" aria-hidden="true"></i><p class="text">Speedlimiter: <strong>'. $speedlimiter .'</strong></p>
+                        '. $police .'
+                      </div>
+                    </div>
+                    </div>
+                ';
+        } 
+    ?>
+        </div>
+    </div>
+    <!--Author Section-->
+    <br><center><a href="https://github.com/SgtBreadStick" class="w" target="_blank">Developed By SgtBreadStick</a></center>
+    <center><p style="font-size: 0.75em;">Ver v2.0</p></center>
+    <br><br>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/holder.js"></script>
+    <script type="text/javascript" src="js/tinymce/tinymce.min.js"></script>
+    <script type="text/javascript" src="js/dataTables.min.js"></script>
+    <script type="text/javascript" src="js/dataTables.bootstrap.min.js"></script>
+</body>
+</html>
